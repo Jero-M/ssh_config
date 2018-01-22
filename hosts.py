b3d1dab0b28e38504d6793636ef2dc6d38744861
Jero-M <jero_maggi@hotmail.com>
2018-01-22 11:42:29 -0800

    Remove selected hosts

--- hosts.py
+++ hosts.py
@@ -30,15 +30,22 @@ class StartUI(QtGui.QMainWindow):
                                QtCore.SIGNAL("clicked()"),
                                self.add_selected)
 
+        QtCore.QObject.connect(self.ui.button_remove,
+                               QtCore.SIGNAL("clicked()"),
+                               self.remove_selected)
+
     def load_default_hostnames_file(self):
         '''Load the default hostnames file'''
         data = []
+        header = []
         with open(default_hostnames, "rb") as csv_file:
             reader = csv.reader(csv_file)
-            for i, column_name in enumerate(reader.next()):
+            header = reader.next()
+            for i, column_name in enumerate(header):
                 if column_name == "Hostname":
                     self.hostname_csv_column = i
                     break
+            data.append(header)
             for row in reader:
                 data.append(row)
             return data
@@ -52,17 +59,29 @@ class StartUI(QtGui.QMainWindow):
 
     def add_all_hostnames_to_list(self):
         '''Load all the hostnames into the list'''
-        for row in self.hostnames_file:
+        for row in self.hostnames_file[1:]:  # [1:] skips the header row
             self.ui.list_hosts.addItem(row[self.hostname_csv_column])
 
     def remove_selected(self):
         '''Remove the selected hostname/s from the list'''
+        selected_hosts_items = self.ui.list_hosts.selectedItems()
+        for item in selected_hosts_items:
+            # Remove from UI
+            index = self.ui.list_hosts.row(item)
+            self.ui.list_hosts.takeItem(index)
+            # Remove from file
+            del self.hostnames_file[index + 1]
+        # Save file
+        self.save_hostnames_to_file()
 
     def add_selected(self):
         '''Add the selected hostname to the list'''
         host_to_add = str(self.ui.text_host.text())
+        # Append to file
         self.hostnames_file.append(['0.0.0.0', '0 ms', host_to_add, '[n/s]'])
+        # Add to UI
         self.ui.list_hosts.addItem(host_to_add)
+        # Save file
         self.save_hostnames_to_file()
 
 
